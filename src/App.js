c1b91de3faf78e078a20a14584ecc1c11c23651e
Karsten Rabe <karstenrabe91@gmail.com>
2019-11-22 20:43:13 -0500

    Changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
-import { Layout, Navigation, Header, Drawer, Content } from 'react-mdl'
+import {
+  Layout,
+  Navigation,
+  Header,
+  Drawer,
+  Content,
+} from 'react-mdl'
 import { Link } from 'react-router-dom'
 import './App.css'
 
 import Main from './pages/Main'
 import Footer from './pages/Footer'
 class App extends Component {
-  render () {
+  render() {
     const closeDrawer = () => {
       const visibleElems = document.querySelectorAll('.is-visible')
       for (let i = 0; i < visibleElems.length; i++) {
@@ -29,22 +35,27 @@ class App extends Component {
             </Navigation>
           </Header>
 
-          <Drawer
-            title={
-              <Link style={{ textDecoration: 'none', color: 'black' }} to='/'>
-                MyPortfolio
-              </Link>
-            }
-            onClick={closeDrawer}
-          >
-            <Navigation>
-              <Link to='/'>Home</Link>
-              <Link to='/projects'>Projects</Link>
-              <Link to='/resume'>Resume</Link>
-              <Link to='/aboutme'>AboutMe</Link>
-              <Link to='/contact'>Contact</Link>
-            </Navigation>
-          </Drawer>
+          <div id="navigation-drawer">
+            <Drawer
+              title={
+                <Link
+                  style={{ textDecoration: 'none', color: 'black' }}
+                  to='/'
+                >
+                  MyPortfolio
+                </Link>
+              }
+              onClick={closeDrawer}
+            >
+              <Navigation>
+                <Link to='/'>Home</Link>
+                <Link to='/projects'>Projects</Link>
+                <Link to='/resume'>Resume</Link>
+                <Link to='/aboutme'>AboutMe</Link>
+                <Link to='/contact'>Contact</Link>
+              </Navigation>
+            </Drawer>
+          </div>
 
           <Content>
             <div className='page-content' />
